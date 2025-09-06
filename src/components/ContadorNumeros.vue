a274f49acc0a1b050f9fbd06988f8d16452f99bb
Pablo-Campuzano <baenapablo2@gmail.com>
2025-09-05 21:51:24 -0500

    Incorporacion de componente ContadorNumeros.vue con una constante

---
+++ src/components/ContadorNumeros.vue
@@ -0,0 +1,29 @@
+<template>
+<h1> Hola Mundo </h1>
+<p>{{ contador }}</p>
+<button @click="Incrementar">Incrementar</button>
+<button @click="Disminuir">Disminuir</button>
+</template>
+
+
+
+<script setup>
+import {ref} from 'vue';
+const contador = ref(0);
+const Incrementar = () => {
+  contador.value++;
+}
+const Disminuir = () => {
+  contador.value--;
+}
+</script>
+
+
+<style scoped>
+p {
+  color:blue;
+}
+h1 {
+  color: green;
+}
+</style>
